47cb9faae89b671180fef1c8d292ded572ec9eb4
reyno405 <reyno405@gmail.com>
2022-10-06 12:29:44 -0400

    this update has employee and roles seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,3 +12,22 @@ CREATE TABLE dapartment (
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- Create table roles
+CREATE TABLE roles (
+    title VARCHAR(30)
+    salary DECIMAL
+    department_id INT
+    PRIMARY KEY (id)
+)
+
+-- Create table employee
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENTT,
+    first_name VARCHAR(30)
+    last_name VARCHAR(30)
+    role_id INT
+    manager_id INT
+    PRIMARY KEY (id)
+)
+
