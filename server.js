087536ac964e2bd8a27735b95c299cb935d61800
reyno405 <reyno405@gmail.com>
2022-10-11 16:58:35 -0400

    this has => to add department

--- server.js
+++ server.js
@@ -158,3 +158,30 @@ function addEmployee() {
             })
     })
 };
+
+// add a department to the database
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                name: 'newDepartment',
+                type: 'input',
+                message: 'Which department would you like to add?'
+            }
+        ]).then(function (answer) {
+            connection.query(
+                'INDERT INTO DEPARTMENT SET ?',
+                {
+                    name: answer.newDepartment
+                });
+            var query = 'SELECT * FROM department';
+            connection.query(query, function (err, res) {
+                if (err) throw err;
+                console.log('Your department has been added!');
+                console.table('All Departments:', res);
+                options();
+            })
+        })
+};
+
+// add a role to the database
